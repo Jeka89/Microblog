991ab0ff788b1ca154f43dddcee435d66dfd2845
Jeka89 <zoof24@gmail.com>
2019-01-26 11:42:53 +0200

    fix conf

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class Config(object):
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+  LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+  
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
